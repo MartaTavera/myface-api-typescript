2e3b8ca3727584cbefbb2a31452c35f48d20acf7
MartaTavera <martatavera@gmail.com>
2024-02-08 17:51:07 +0000

    Create new user validation

--- myface-frontend/src/CreateUser.tsx
+++ myface-frontend/src/CreateUser.tsx
@@ -1,30 +1,111 @@
+import { CreateUserRequest } from '../../src/models/api/createUserRequest'
+import { useState, FormEventHandler, ChangeEventHandler } from "react";
 
-export default function CreateUser(){
 
-    return(
-        <div>
+function ErrorDiv(props: any) {
+    if (props.isValidated === false) {
+        return (
+            <div>{props.errors}</div>
+        )
+    }
+}
+
+
+export default function CreateUser() {
+    const [user, setUser] = useState<CreateUserRequest>({
+        name: '',
+        username: '',
+        email: '',
+        profileImageUrl: '',
+        coverImageUrl: '',
+    })
+
+
+    const [isValidated, setIsValidated] = useState<boolean>(true);
+    const [errors, setErrors] = useState<string>();
+
+
+    const validateInput = (user: any) => {
+        let error: string = '';
+
+        if (user.username.length < 2) {
+            error = "The username is too short";
+            setErrors(error);
+            setIsValidated(false);
+        }
+        if (user.email === '') {
+            error = "enter an email address";
+            setErrors(error);
+            setIsValidated(false);
+        }
+    }
+
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+        const { name, value } = event.target;
+        setUser(prev => ({ ...prev, [name]: value }))
+    }
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        console.log(user);
+        validateInput(user);
+
+        if (isValidated === true) {
+            fetch("http://localhost:3001/users/create", {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user),
+            })
+                .then((response) => {
+                    if (response.status !== 200) {
+                        throw new Error(response.statusText);
+                    }
+                })
+                .then(() => {
+                    console.log("We'll be in touch soon.");
+                })
+                .catch((err) => {
+                    console.log(err.toString());
+                });
+        }
+    };
+
+    return (
+        <div className="flexContainer">
             <h1>Create User</h1>
-            <form method="post" action="/users/create">
-                <label>
-                    Name:
-                    <input type="text" name="name"/>
-                </label>
-                <label>
-                    Username:
-                    <input type="text" name="username"/>
-                </label>
-                <label>
-                Email:
-                    <input type="text" name="email"/>
-                </label>
-                <label>
-                ProfileImageUrl:
-                    <input type="text" name="profileImageUrl"/>
-                </label>
-                <label>
-                CoverImageUrl:
-                    <input type="text" name="coverImageUrl"/>
-                </label>
+            <ErrorDiv errors={errors} isValidated={isValidated} />
+            <form onSubmit={handleSubmit}>
+                <div className="formContainer">
+                    <label>
+                        Name:
+                        <br />
+                        <input type="text" name="name" value={user.name} onChange={handleChange} />
+                    </label>
+                    <label>
+                        Username:
+                        <br />
+                        <input type="text" name="username" value={user.username} onChange={handleChange} />
+                    </label>
+                    <label>
+                        Email:
+                        <br />
+                        <input type="text" name="email" value={user.email} onChange={handleChange} />
+                    </label>
+                    <label>
+                        ProfileImageUrl:
+                        <br />
+                        <input type="text" name="profileImageUrl" value={user.profileImageUrl} onChange={handleChange} />
+                    </label>
+                    <label>
+                        CoverImageUrl:
+                        <br />
+                        <input type="text" name="coverImageUrl" value={user.coverImageUrl} onChange={handleChange} />
+                    </label>
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
--- myface-frontend/src/PostList.tsx
+++ myface-frontend/src/PostList.tsx
@@ -1,20 +1,72 @@
 // import React from "react";
 import { useState, useEffect } from 'react'
 import './style/App.scss'
+import {Page} from '../../src/models/api/page'
+import {PostModel} from '../../src/models/api/postModel'
+
+import { FormEventHandler, ChangeEventHandler } from "react";
 
 
-function PostList() {
-  const [myData, setMyData] = useState<any>(null);
+  export default function PostList(){
+    const [myData, setMyData] = useState<Page<PostModel>|null>(null);
 
-  useEffect(() => {
-    fetch("http://localhost:3001/posts")
-      .then(response => response.json())
-      .then(data => setMyData(data));
-  }, []);
+    useEffect(() => {
+    fetch("http://localhost:3001/posts").then(response => response.json()).then(data => setMyData(data));
+    }, [])
+    
+    if(!myData){
+        return <h1>Waiting for data</h1>
+    }
 
-  if (!myData) {
-    return <div>Waiting for data!</div>
-  }
+    let likes : string[] = [];
+    let dislikes : string[] = [];
+
+    myData.results.forEach((post) =>
+        post.likedBy.forEach((user) => {
+            likes.push(user.username)
+            }
+        )
+    )
+    myData.results.forEach((post) =>
+        post.dislikedBy.forEach((user) => {
+            dislikes.push(user.username)
+            }
+        )
+    )
+
+    return (
+        <div className="flexContainer">
+            <h1 className="subtitle">Posts</h1>
+            <div className="postsContainer">
+            {myData.results.map((post) =>
+                <div className="postContainer" key={post.id}>
+                    <img className="postImage" src={post.imageUrl}></img>
+                    <div className ="postInfo">
+                        <h3>{post.message}</h3>
+                        <p>By {post.postedBy.username}</p>
+                        <p>{post.createdAt}</p>
+                        <p>Liked by: {likes.length}</p>
+                        <p>{likes.join(', ')}</p>
+                        <p>Disliked by: {dislikes.length}</p>
+                        <p>{dislikes.join(', ')}</p>
+                    </div>
+                    <div className= "postButtonContainer">
+                        <form method="post">
+                            <button type="submit">Dislike</button>  
+                        </form>
+                        <form method="post" action="/posts/{post.id}/like">
+                            <button type="submit">Like</button>  
+                        </form>
+                    </div>
+                </div>
+            )}
+            </div>
+        </div>
+    )
+}
+
+
+/*
 
   return (
     <div className="flexContainer">
@@ -56,3 +108,4 @@ function PostList() {
 }
 
 export default PostList;
+*/
